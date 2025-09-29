c2ab814a886481624150f1e9c2c45fbd4febea52
Jefferson Cristian Tertuliano Cavalcante Ferreira <jtertuli@c1r1p4.42sp.org.br>
2025-09-29 11:44:55 -0300

    feat: start new version with monitor of die

---
+++ philo v2/philosophers.h
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/28 08:29:59 by jtertuli          #+#    #+#             */
+/*   Updated: 2025/09/29 11:12:30 by jtertuli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# define _DEFAULT_SOURCE
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <sys/time.h>
+
+# include "../libft/libft.h"
+
+# define RED_BOLD "\033[1;31m"
+# define RED "\33[31m"
+# define GREEN "\33[32m"
+# define YELLOW "\33[33m"
+# define BLUE "\33[36m"
+# define END_COLOR "\033[0m"
+
+typedef struct s_common {
+	int				number_of_philosophers;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	long			number_of_times_must_eat;
+	int				shutdown;
+	pthread_mutex_t	*forks_mutex;
+	pthread_mutex_t	shutdown_mutex;
+	pthread_mutex_t	printf_mutex;
+	pthread_mutex_t	last_meal_mutex;
+}	t_common;
+
+typedef struct s_philo {
+	int				id_philo;
+	struct timeval	last_meal;
+	t_common		*common;
+}	t_philo;
+
+// phipholophers.c
+void	*ft_core(void *args);
+void	ft_start(t_common **common);
+void	ft_create(pthread_t	*thr_philos, t_philo *philos, t_common *common);
+
+// utils.c
+void	ft_free_common(t_common **common);
+void	ft_destroy_mutex(t_common *common);
+int		ft_validation(int argc, char *argv[]);
+void	ft_join(int quantity, pthread_t *thr_philos);
+
+// utils_init.c
+int		ft_init_mutex(t_common **common);
+int		ft_init_common(int argc, char *argv[], t_common **common);
+
+#endif
---
+++ philo v2/utils.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtertuli <jtertuli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/28 08:21:34 by jtertuli          #+#    #+#             */
+/*   Updated: 2025/09/28 17:19:50 by jtertuli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	ft_validation(int argc, char *argv[])
+{
+	int	i;
+
+	if (argc == 1 || (argc < 5 || argc > 6))
+		return (1);
+	i = 1;
+	while (i < argc)
+	{
+		if (!ft_is_only_number(argv[i]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_join(int quantity, pthread_t *thr_philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < quantity)
+	{
+		pthread_join(thr_philos[i], NULL);
+		i++;
+	}
+}
+
+void	ft_destroy_mutex(t_common *common)
+{
+	int	i;
+
+	i = 0;
+	while (i < common->number_of_philosophers)
+	{
+		pthread_mutex_destroy(&common->forks_mutex[i]);
+		i++;
+	}
+	pthread_mutex_destroy(&common->printf_mutex);
+	pthread_mutex_destroy(&common->shutdown_mutex);
+}
+
+void	ft_free_common(t_common **common)
+{
+	ft_to_free((void **) &(*common)->forks_mutex);
+	ft_to_free((void **) common);
+}
